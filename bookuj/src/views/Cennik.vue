30430fddb88ade661c7f1220f3f43ba7c52dd944
Magdalena Stopa <magdastopa23@gmail.com>
2023-05-24 18:39:03 +0200

    feat(css/pricing): Changed css, add git ignore

--- bookuj/src/views/Cennik.vue
+++ bookuj/src/views/Cennik.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        <h1>Cennik</h1>
+    <div class="main-container">
+      <h1 class="h1-pricing">Cennik</h1>
+      <div class="image-container">
+        <img src="../../assets/left_pricing_ss.png" alt="Image 1" class="price-image" />
+        <img src="../../assets/right_pricing_ss.png" alt="Image 2" class="price-image" />
+      </div>
     </div>
-  </template>
+</template>
--- bookuj/src/views/Kontakt.vue
+++ bookuj/src/views/Kontakt.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         <h1>Kontakt</h1>
+        <h1>KontDakt</h1>
+
     </div>
   </template>
