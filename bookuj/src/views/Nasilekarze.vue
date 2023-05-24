c355989b6e5c2fe8a2daa88eaf87ad94d2856de0
Magdalena Stopa <magdastopa23@gmail.com>
2023-05-24 19:29:27 +0200

    feat(): nasilekarze

--- bookuj/src/views/Nasilekarze.vue
+++ bookuj/src/views/Nasilekarze.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Nasilekarze</h1>
+
+<div class="main-container">
+    <h1 class="h1-pricing">Nasi Lekarze</h1>
+    <div class="image-container">
+      <img src="../../assets/1_doctor_ss.png" alt="Image 1" class="doc-image" />
+      <img src="../../assets/2_doctor_ss.png" alt="Image 2" class="doc-image" />
+      <img src="../../assets/3_doctor_ss.png" alt="Image 3" class="doc-image" />
     </div>
-  </template>
+  </div>
+</template>
