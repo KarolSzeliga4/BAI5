55ef54949bc3021c824feca51f4cc3f20f802f3d
Magdalena Stopa <magdastopa23@gmail.com>
2023-05-26 20:18:44 +0200

    feat(css): changed css

--- bookuj/src/views/Zakresuslug.vue
+++ bookuj/src/views/Zakresuslug.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        <h1>Zakresuslug</h1>
+   <div class="main-container">
+      <h1 class="h1-views">Zakres usług</h1>
+
+    
+        <div class="grid-wrapper2">
+                <div class=".lekarze drClooney"><br><br><br><br><br><br><br><br><br><br><br><br>Kardiologia Konsultacje: <br><br>Bóle w 
+                    klatce piersiowej <br>Nadciśnienie Niewydolność <br>O
+                    
+                    pieka po zawale <br>Ocena ryzyka przed operacjami <br><br>Diagnostyka <br><br>Echokardiografia <br>EKG <br>Holter <br>Próba wysiłkowa</div>
+                     
+                <div class=".lekarze drZosia"><br><br><br><br><br><br><br><br><br><br><br><br>Ortopedia<br><br> Konsultacje lekarskie:<br><br> 
+                    Bóle chrząstno Stawowe<br>Kwalifikacje do zabiegów <br>Blokada/punkcja/nakłucie <br><br>Diagnostyka <br>USG <br>
+                    <br>Zabiegi małoinwazyjne 
+                    <br>Zastosowanie osocza 
+                    <br>Iniekcja kwasu hialuronowego <br>Usunięcie zmiany skórnej do 1,5 cm </div>
+                <div class=".lekarze drHandsome"><br><br><br><br><br><br><br><br><br><br><br><br>Internista <br><br>
+                    Konsultacje lekarskie <br>
+                    Poradnia chorób wewnętrznych <br>
+                    Diagnostyka stanu zdrowia <br>
+                    Ocena stanu zdrowia <br>
+                    Skierowanie do lekarza specjalisty</div>
+         </div>           
+               
     </div>
+    <div class="image-container">
+        <img src="../../assets/dr_clooney.png" alt="Image 4" class="round-image4" />
+        <img src="../../assets/dr_zosia.png" alt="Image 5" class="round-image5" />
+        <img src="../../assets/dr_handsome.png" alt="Image 6" class="round-image6" />
+    </div>
+
   </template>
