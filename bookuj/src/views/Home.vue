3621aed535e212a581b922560841c3b01445c03b
maciejpacun.maciej <maciejpacun@gmail.com>
2023-06-09 14:33:39 +0200

    piatek

--- bookuj/src/views/Home.vue
+++ bookuj/src/views/Home.vue
@@ -6,18 +6,29 @@
     
         <div class="grid-wrapper2">
                     <div class="strona startowa1">
-                        <h1 class="h1home">Najlepsi lekarze!</h1>
-                        <h2 class="h2home">W trosce o Twoje zdrowie i dobre samopoczucie!</h2>
+                        <h1 class="h1home">Poradnie specjalistyczne</h1>
+                        <h2 class="h2home">Konsultacje lekarskie</h2>
+                        <img src="../../assets/serce2.png" class="ikonka1">
+                        <h1 class="h1home">Diagnostyka</h1>
+                        <h2 class="h2home">Wyniki badań</h2>
+                        <img src="../../assets/staw.png" class="ikonka1">
                     </div>
                     <div class="strona startowa2">
-                        <h1 class="h1home">Nowoczesny sprzęt!</h1>
-                        <h2 class="h2home">Wykorzystany do leczenia i profilaktyki!</h2>
+                        <h1 class="h1home">Zabiegi małoinwazyjne</h1>
+                        <h2 class="h2home">Mniej bólu, szybsze efekty</h2>
+                        <img src="../../assets/chart.png" class="ikonka1">
+                        <h1 class="h1home">Portal pacjenta</h1>
+                        <h2 class="h2home">wszystkie wizyty i wyniki badań w jednym miejscu</h2>
+                        <img src="../../assets/osoby.png" class="ikonka1">
                     </div>
                     <div class="strona startowa3">
-                        <h1 class="h1home">Nie czekaj!</h1>
-                        <h2 class="h2home">Zarezerwuj wizytę już teraz!</h2>
+                        <h1 class="h1home">Badania diagnostyczne</h1>
+                        <h2 class="h2home">Badania obrazowe i labolatoryjne</h2>
+                        <img src="../../assets/headset.png" class="ikonka1">
+                        <h1 class="h1home">Infolinia</h1>
+                        <h2 class="h2home">Całodobowa</h2>
+                        <img src="../../assets/stetoskop.png" class="ikonka1">
                     </div>
-                   
         </div></div>
 </template>
 
